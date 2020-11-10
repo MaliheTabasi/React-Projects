e3f47da6f7b8048e5d3273a35bec70328f8388c4
Malihe <maliheh73@gmail.com>
2020-11-10 14:29:53 +0330

    movie-collection: pagination added

--- movie-collection/src/components/movies.jsx
+++ movie-collection/src/components/movies.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService'
 import Like from './common/like';
+import Pagination from './common/pagination';
+import {paginate} from '../utils/paginate';
 
 class Movie extends Component {
     state = { 
-        movies : getMovies()
+        movies : getMovies(),
+        pageSize: 4,
+        currentPage:1
      }
 
+    handleDelete = movie => {
+        const movies = this.state.movies.filter(m=>m._id !== movie._id)
+        this.setState({movies})
+    }
+    
     handleLike= (movie)=> {
         const movies = [...this.state.movies];
         const index = movies.indexOf(movie);
@@ -15,13 +24,18 @@ class Movie extends Component {
         this.setState({movies})
     }
 
-    handleDelete = movie => {
-        const movies = this.state.movies.filter(m=>m._id !== movie._id)
-        this.setState({movies})
+    handlePageChange= page => {
+        this.setState({currentPage:page})
     }
+
     render() { 
         const { length: count } = this.state.movies;
+        const { pageSize, currentPage, movies: allMovies } = this.state;
+
         if(count ===0) return <p>There are no movies in database!</p>;
+        
+        const movies = paginate(allMovies, currentPage, pageSize);
+        
         return (                  
                 <div>
                 <p>Showing {count} movies in the database.</p>
@@ -37,7 +51,7 @@ class Movie extends Component {
                     </tr>
                     </thead>
                     <tbody>  
-                        {this.state.movies.map( movie => 
+                        {movies.map( movie => 
                             <tr key={movie._id}>
                                 <td>{movie.title}</td>
                                 <td>{movie.genre.name}</td>
@@ -51,6 +65,7 @@ class Movie extends Component {
                     }
                     </tbody> 
                 </table>
+                <Pagination itemsCount={count} pageSize={pageSize} onPageChange={this.handlePageChange} currentPage={currentPage}/>
                 </div>            
          );
     }
