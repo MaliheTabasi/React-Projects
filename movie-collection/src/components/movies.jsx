e44fccce8b4d4da2cf1ac5eed2fa6f2c3f6d6ba0
Malihe <maliheh73@gmail.com>
2020-11-11 22:58:05 +0330

    movie-collection:  code refactoring and comp extraction done!

--- movie-collection/src/components/movies.jsx
+++ movie-collection/src/components/movies.jsx
@@ -45,14 +45,10 @@ class Movie extends Component {
     handleSort = sortColumn=> {
         this.setState({sortColumn})
     }
-
-    render() { 
-        const { length: count } = this.state.movies;
+    getPageData(){
         const { pageSize, currentPage, selectedGenre,
-             movies: allMovies, sortColumn } = this.state;
+            movies: allMovies, sortColumn } = this.state;
 
-        if(count ===0) return <p>There are no movies in database!</p>;
-        
         const filtered = selectedGenre && selectedGenre._id
         ?allMovies.filter(m=>m.genre._id===selectedGenre._id)
         : allMovies;
@@ -60,6 +56,17 @@ class Movie extends Component {
         const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
 
         const movies = paginate(sorted, currentPage, pageSize);
+        return{ totalCount: filtered.length, data: movies}
+    };
+
+    render() { 
+        const { length: count } = this.state.movies;
+        const { pageSize, currentPage,
+             sortColumn } = this.state;
+
+        if(count ===0) return <p>There are no movies in database!</p>;
+        
+        const {totalCount, data:movies}= this.getPageData();
         
         return (                  
                 <div className='row'>
@@ -71,14 +78,14 @@ class Movie extends Component {
                         />
                     </div>
                     <div className="col">
-                        <p>Showing {filtered.length} movies in the database.</p>
+                        <p>Showing {totalCount} movies in the database.</p>
                         <MoviesTable 
                             movies={movies} 
                             onLike={this.handleLike} 
                             onDelete={this.handleDelete}
                             onSort={this.handleSort} 
                             sortColumn={sortColumn}/>
-                        <Pagination itemsCount={filtered.length} pageSize={pageSize} onPageChange={this.handlePageChange} currentPage={currentPage}/>
+                        <Pagination itemsCount={totalCount} pageSize={pageSize} onPageChange={this.handlePageChange} currentPage={currentPage}/>
                     </div>
                 
                 </div>            
--- movie-collection/src/components/moviesTable.jsx
+++ movie-collection/src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Like from './common/like';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
+import Table from './common/table';
 
 class MoviesTable extends Component {
     columns=[
@@ -18,18 +17,12 @@ class MoviesTable extends Component {
             onClick={()=>this.props.onDelete(movie)}
             className='btn btn-danger btn-sm'>Delete</button>},
     ];
+
     render() { 
         const { movies, onSort, sortColumn} = this.props;
 
         return ( 
-            <table className='table'>
-            <TableHeader columns={this.columns} 
-                sortColumn={sortColumn}
-                onSort={onSort}
-            />
-            <TableBody data={movies} columns={this.columns}/>
-            
-            </table>
+            <Table columns={this.columns} sortColumn={sortColumn} onSort={onSort} data={movies}/>
          );
     }
 }
