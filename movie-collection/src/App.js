928616106faeb7d9c0db0e20c9fe6be2b5e0749e
Malihe <maliheh73@gmail.com>
2020-11-12 20:56:36 +0330

    Routing using react-router-dom addded

--- movie-collection/src/App.js
+++ movie-collection/src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBAr from './components/navBar';
+import MovieForm from './components/movieForm';
+import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <main className='container'>
-                <Movies />
-            </main>
+            <React.Fragment>
+                <NavBAr />
+                <main className='container'>
+                    <Switch>
+                        <Route path='/movies/:id' component={MovieForm} />
+                        <Route path='/movies' component={Movies} />
+                        <Route path='/rentals' component={Rentals} />
+                        <Route path='/customers' component={Customers} />
+                        <Route path='/not-found' component={NotFound} />
+                        <Redirect from='/' exact to='/movies' />
+                        <Redirect to='/not-found' />
+                    </Switch>
+                </main>
+            </React.Fragment>
         );
     }
 }
