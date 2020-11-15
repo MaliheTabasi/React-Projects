b1713f5bcfa50b0b039dae9c940140a982811f1d
Malihe <maliheh73@gmail.com>
2020-11-15 18:41:58 +0330

    http and logging services added

---
+++ movie-collection/src/services/logService.js
@@ -0,0 +1,21 @@
+// import Raven from 'raven-js';
+
+function init() {
+    // Raven.config(
+    //     'https://051aaf9bbbe14d2287e89e9a515c2ba7@o476760.ingest.sentry.io/5516853',
+    //     {
+    //         release: '1-0-0',
+    //         environment: 'development-test',
+    //     }
+    // ).install();
+}
+
+function log(error) {
+    // Raven.captureException(error);
+    console.log('hogger service::', error);
+}
+
+export default {
+    init,
+    log,
+};
